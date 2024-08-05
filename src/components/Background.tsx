669586f5b177622aacef8d627f5c548a92748735
porkieeeeeee <porkieeeeeee@gmail.com>
2024-08-05 16:55:03 +0900

    feat::3d 컴포넌트 추가

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,6 +3,9 @@ import { OrbitControls } from "@react-three/drei";
 import { useEffect, useState } from "react";
 import * as THREE from "three";
 import styled from "@emotion/styled";
+import Stone from "./Stone";
+import House from "./House";
+import Tree from "./Tree";
 
 const Background = () => {
   const [texture, setTexture] = useState<THREE.Texture | null>(null);
@@ -16,7 +19,24 @@ const Background = () => {
         loadedTexture.wrapS = THREE.RepeatWrapping;
         loadedTexture.wrapT = THREE.RepeatWrapping;
         loadedTexture.repeat.set(10, 10);
-        loadedTexture.offset.set(0, 0);
+        setTexture(loadedTexture);
+      },
+      undefined,
+      (error) => {
+        console.error("Error loading texture:", error);
+      }
+    );
+  };
+
+  useEffect(() => {
+    const loader = new THREE.TextureLoader();
+
+    loader.load(
+      "/models/grass/textures/tex_u1_v1_baseColor.jpeg",
+      (loadedTexture) => {
+        loadedTexture.wrapS = THREE.RepeatWrapping;
+        loadedTexture.wrapT = THREE.RepeatWrapping;
+        loadedTexture.repeat.set(10, 10);
         setTexture(loadedTexture);
       },
       undefined,
@@ -24,21 +44,25 @@ const Background = () => {
         console.error("에러🥵🥵:", error);
       }
     );
-  };
-
-  useEffect(() => {
-    handleTextureLoader();
   }, []);
 
   return (
     <StyleCanvas camera={{ position: [0, 5, 10], fov: 75 }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} />
+      <ambientLight intensity={0.8} />
+      <pointLight position={[10, 10, 10]} intensity={1} color="#ffffff" />
 
       <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[10, 10]} />
         <meshStandardMaterial map={texture} />
       </mesh>
+
+      <Tree position={[0, 0, -3]} />
+      <Tree position={[0, 0, 0]} />
+      <Tree position={[0, 1.2, -3]} />
+      <Stone position={[0.3, -0.143, 3.8]} />
+      <Stone position={[0.3, -0.143, 1.8]} />
+      <Stone position={[0.5, -0.143, -1.3]} />
+      <House />
 
       <OrbitControls />
     </StyleCanvas>
